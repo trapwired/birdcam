daeeec63563337e87bd83c8f8a2bb5bce553deb9
Dominic Weibel <dominic.weibel@digitecgalaxus.ch>
2023-12-27 18:52:40 +0100

    Finish model integration

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import time
 import cv2
 import imutils
 
+from CnnModel import CnnModel
+
 IMAGE_COUNTER = 0
-TIMEOUT = 1
+TIMEOUT = 0.3
 SENSITIVITY = 2000000
 
 
@@ -24,40 +26,32 @@ def return_camera_indexes():
     return arr
 
 
+def detect_birds(capture_device, cnn_model, output_directory_path):
+    global IMAGE_COUNTER
+    while True:
+        _, start_frame = capture_device.read()
+        rotated_image = cv2.rotate(start_frame, cv2.ROTATE_180)
+        try:
+            res = cnn_model.infer_image(rotated_image)
+        except Exception:
+            break
+        if res:
+            img_name = "bird_picture_{}.png".format(IMAGE_COUNTER)
+            path = os.path.join(output_directory_path, img_name)
+            cv2.imwrite(path, rotated_image)
+            IMAGE_COUNTER += 1
+            time.sleep(TIMEOUT)
+            if IMAGE_COUNTER > 2000:
+                exit()
+
+
 if __name__ == '__main__':
+    model = CnnModel()
     cap = cv2.VideoCapture(0)
-
-    ret, start_frame = cap.read()
-
-    start_frame = imutils.resize(start_frame, width=500)
-    start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
-    start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
 
     # create new folder for each run
     dir_name = time.strftime("%Y%m%d-%H%M%S")
     dir_path = os.path.join('out', dir_name)
     os.mkdir(dir_path)
 
-    while True:
-
-        _, full_frame = cap.read()
-        frame = imutils.resize(full_frame, width=500)
-
-        frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
-
-        difference = cv2.absdiff(start_frame, frame_bw)
-        threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
-        start_frame = frame_bw
-
-        if threshold.sum() > SENSITIVITY:
-            print("motion detected")
-            img_name = "opencv_frame_{}.png".format(IMAGE_COUNTER)
-            path = os.path.join(dir_path, img_name)
-            rotated_image = cv2.rotate(full_frame, cv2.ROTATE_180)
-            cv2.imwrite(path, rotated_image)
-            IMAGE_COUNTER += 1
-            time.sleep(TIMEOUT)
-
-    cap.release()
-    cv2.destroyAllWindows()
+    detect_birds(cap, model, dir_path)
