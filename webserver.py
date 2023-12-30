a5733182637aa27a6cb48cbf5f505c3b40d93f1d
Dominic Weibel <dominic.weibel@digitecgalaxus.ch>
2023-12-30 14:39:49 +0100

    add picture-taking logic

--- webserver.py
+++ webserver.py
@@ -1,4 +1,10 @@
+import time
+
 from flask import Flask
+import RPi.GPIO as GPIO
+
+focus_pin = 23
+picture_pin = 24
 
 app = Flask(__name__)
 
@@ -6,8 +12,18 @@ app = Flask(__name__)
 @app.route('/take_picture', methods=['GET'])
 def take_picture():
     print('Take Picture')
+    GPIO.output(focus_pin, True)
+    time.sleep(0.1)
+    GPIO.output(picture_pin, True)
+    time.sleep(0.1)
+    GPIO.output(focus_pin, False)
+    GPIO.output(focus_pin, False)
     return 'Picture Taken!'
 
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(focus_pin, GPIO.OUT)
+    GPIO.setup(picture_pin, GPIO.OUT)
+
     app.run(host='0.0.0.0', threaded=True, port=5000)
